303200d5c170724012dc8cd2fc18fbd86dfa3ab2
Kevin Boschetti <65442174+ItzBosk@users.noreply.github.com>
2024-01-31 19:56:03 +0100

    pit test
    boh mi sono trovato meaven pure

---
+++ ews_TVS/mutation_testing/src/mutation/Example.java
@@ -0,0 +1,18 @@
+package mutation;
+
+public class Example {
+
+	// calcola b^e
+	int power(int b, int e) throws Exception {
+		if (e < 0)
+			throw new Exception("Negative exponent");
+		if ((b == 0) && (e == 0))
+			throw new Exception("Undefined");
+		int r = 1;
+		while (e > 0) {
+			r = r * b;
+			e = e - 1;
+		}
+		return r;
+	}
+}
